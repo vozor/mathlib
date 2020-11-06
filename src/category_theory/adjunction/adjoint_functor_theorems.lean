6c5876e28f3be2a7f9ba669225c01525210e7e2a
Bhavik Mehta <bhavikmehta8@gmail.com>
2020-11-06 01:04:52 +0000

    fix module doc

--- src/category_theory/adjunction/adjoint_functor_theorems.lean
+++ src/category_theory/adjunction/adjoint_functor_theorems.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2019 Reid Barton. All rights reserved.
+Copyright (c) 2020 Bhavik Mehta. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Reid Barton, Johan Commelin
+Authors: Bhavik Mehta
 -/
 import category_theory.adjunction.basic
 import category_theory.limits.shapes
